34a7605aab0cb06b91420e445d8f2e349388b943
Yanto <yanto3528@gmail.com>
2019-11-23 14:06:53 +0700

    added button, form input, and sign in page

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -3,13 +3,17 @@ import "./Homepage.scss";
 import Directory from "../../components/directory/Directory";
 import ShopPage from "../shop-page/ShopPage";
 import { Switch, Route } from "react-router-dom";
+import Header from "../../components/header/Header";
+import SignInSignUpPage from "../sign-in-sign-up-page/SignInSignUpPage";
 
 const Homepage = () => {
   return (
     <div className="homepage">
+      <Header />
       <Switch>
         <Route exact path="/" component={Directory} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
