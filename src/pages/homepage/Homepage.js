d75f6780a66d49a1cba468e655b3e3776297f990
Yanto <yanto3528@gmail.com>
2019-11-23 08:46:54 +0700

    implement react router for menu item with withRouter HOC

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Homepage.scss";
 import Directory from "../../components/directory/Directory";
+import { Switch, Route } from "react-router-dom";
+
+const HatPage = () => {
+  return <h1>Hat Page</h1>;
+};
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Directory />
+      <Switch>
+        <Route exact path="/" component={Directory} />
+        <Route exact path="/shop/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 };
