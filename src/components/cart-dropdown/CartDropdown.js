e64cf7bc608c97c5ec0216e28a6505d43ef1308e
Yanto <yanto3528@gmail.com>
2019-11-26 08:56:25 +0700

    implemented total cartItemCount and adding reselect to cart dropdown and cart icon

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import CustomButton from "../custom-button/CustomButton";
 import "./CartDropdown.scss";
 import CartItem from "../cart-item/CartItem";
+import { selectCartItems } from "../../redux/cart/cartSelector";
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -15,8 +16,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
